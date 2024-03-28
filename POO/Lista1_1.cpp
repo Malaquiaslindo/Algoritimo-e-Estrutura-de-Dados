bc9d2264dad05abda9e4e505b7561773fc51e13d
Ian Lucas <137118897+Malaquiaslindo@users.noreply.github.com>
2024-03-28 11:48:06 -0300

    Update Lista1_1.cpp

--- POO/Lista1_1.cpp
+++ POO/Lista1_1.cpp
@@ -1,1 +1,34 @@
+#include <iostream>
+using namespace std;
+
+class Agenda{
+	
+	public:
+		Agenda(string oNome,int oTelefone){
+		this->Nome = oNome;
+		this->Telefone = oTelefone;
+		
+};
+
+string getNome(){
+	return Nome;
+}
+int getTelefone(){
+	return Telefone;
+}
+void imprimirDados(){
+	cout<< "O seu nome eh "<<Nome;
+	cout<< "\n o seu telefone eh "<<Telefone;
+}
+	private:
+		int Telefone;
+		string Nome;
+};
+
+int main(){
+	Agenda yAgenda("ian",12345);
+	yAgenda.imprimirDados();
+	return 0;
+}
+	
 
