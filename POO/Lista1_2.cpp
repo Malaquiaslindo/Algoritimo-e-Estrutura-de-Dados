17d5418ec70f03995388e63ffa8b80dbc4c15677
Ian Lucas <137118897+Malaquiaslindo@users.noreply.github.com>
2024-04-03 11:00:48 -0300

    Update Lista1_2.cpp

--- POO/Lista1_2.cpp
+++ POO/Lista1_2.cpp
@@ -1,1 +1,17 @@
+| 1 - Conceitue e exemplifique os seguintes itens: 
+a) Programacao Orientada a Objetos (POO)
+  Basei em uma linguagem de programação que é para objetos, que se dividem em abstração, encapsulamento, herança, polimorfia
 
+b) Classe 
+A partir das classes você define o objeto
+
+c) Instancia  
+instancia é a mesma coisa que colocar o objeto na memoria 
+
+
+d) Objeto 
+objeto é aquilo que você pega e classifica assim como você classifica um carro como um automotor
+
+  
+2 - Qual a diferenca entre Struct e Class? Inclua em sua resposta os |
+Modificadores de Visibilidade (+publico, -privado, #protegido). |
